9020fdbc741509a324d9b322690bf31dcd633488
Elliott Hall <elliott.hall@kcl.ac.uk>
2020-06-09 15:26:56 +0100

    Admin fields update

--- field_timeline/admin.py
+++ field_timeline/admin.py
@@ -1,13 +1,29 @@
+rom
+django.contrib
+import admin
+
+# Register your models here.
+from field_timeline.models import (FieldTimelineEvent, FieldTimelineResource,
+                                   FieldTimelineCategory)
+
 from django.contrib import admin
 
 # Register your models here.
-from field_timeline.models import (FieldTimelineEvent, FieldTimelineResource,FieldTimelineCategory)
+from field_timeline.models import (FieldTimelineEvent, FieldTimelineResource,
+                                   FieldTimelineCategory)
 
 
 class FieldTimelineEventAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('start_date_year', 'unique_id', 'headline', 'text')
+    search_fields = ('start_date_year', 'unique_id', 'headline', 'text')
+
+
+class FieldTimelineResourceAdmin(admin.ModelAdmin):
+    list_display = (
+    'resource_id', 'image_ref', 'caption', 'photographer', 'link_url')
+    search_fields = ('resource_id')
 
 
 admin.site.register(FieldTimelineEvent, FieldTimelineEventAdmin)
-admin.site.register(FieldTimelineResource)
+admin.site.register(FieldTimelineResource, FieldTimelineResourceAdmin)
 admin.site.register(FieldTimelineCategory)
