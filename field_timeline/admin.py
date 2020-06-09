8649d7586efc4f2bdd4a1b4e0c1a0d864011b109
Elliott Hall <elliott.hall@kcl.ac.uk>
2020-06-09 14:38:49 +0100

    field timeline event admin

--- field_timeline/admin.py
+++ field_timeline/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from field_timeline.models import (FieldTimelineEvent,FieldTimelineResource)
+
+
+class FieldTimelineEventAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(FieldTimelineEvent,FieldTimelineEventAdmin)
