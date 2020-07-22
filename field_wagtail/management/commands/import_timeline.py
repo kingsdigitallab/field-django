46f6afa95bfde553be17fd1a593fe4666556e2be
elliotthall <elliott.hall@kcl.ac.uk>
2020-07-22 10:40:55 +0000

    import timeline now import command with oral history

---
+++ field_wagtail/management/commands/import_timeline.py
@@ -0,0 +1,285 @@
+"""
+Import FIELD timeline events from csv
+
+KDL (EH) 3/6/2020
+
+"""
+import argparse
+import csv
+from typing import Iterator
+
+from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.files import File
+from django.core.files.images import ImageFile
+from django.core.management.base import BaseCommand
+from dublincore_resource.models import (
+    DublinCoreRights,
+    DublinCoreAgent
+)
+from wagtail.images.models import Image
+
+from field_wagtail.models import (
+    FieldTimelineEvent, FieldOralHistory,
+    FieldTimelineResource, FieldTimelineCategory, FieldTimelineResourceImage
+)
+
+
+def create_category(category_name):
+    category, created = FieldTimelineCategory.objects.get_or_create(
+        category_name=category_name)
+    return category
+
+
+class Command(BaseCommand):
+    help = 'Import FIELD timeline events from csv'
+    verbose = 1  # show logging on screen
+
+    min_row_length = 16
+    events_created = 0
+    events_updated = 0
+    event_links = {}
+
+    @staticmethod
+    def import_rights(rights_file: Iterator[str]) -> str:
+        """ Import rights tab into Dublin Core Rights object"""
+        # rights_file = csv.reader(rightscsvfilename, delimiter=',')
+        result = ''
+        total_rights = 0
+        for rights_line in rights_file:
+            if rights_line and len(rights_line) == 2:
+                DublinCoreRights.objects.get_or_create(
+                    shorthand=rights_line[0],
+                    statement=rights_line[1]
+                )
+                total_rights += 1
+            else:
+                result += "Bad rights line in csv: {}\n".format(rights_line)
+        return result + "Total rights added/update {}\n".format(total_rights)
+
+    def link_events(self) -> None:
+        """Link together events based on dict built from csv"""
+
+        for linker_id in self.event_links:
+            # Get the event we're linking other events to
+            linker_event = None
+            try:
+                linker_event = FieldTimelineEvent.objects.get(
+                    unique_id=linker_id
+                )
+            except ObjectDoesNotExist:
+                self.stdout.write("Linker event not found! ID P{}".format(
+                    linker_id)
+                )
+            if linker_event:
+                for linkee_id in self.event_links[linker_id]:
+                    try:
+                        # pdb.set_trace()
+                        linkee_event = FieldTimelineEvent.objects.get(
+                            unique_id=linkee_id.strip()
+                        )
+                        linker_event.linked_events.add(linkee_event)
+                        if self.verbose == 1:
+                            self.stdout.write(
+                                "Event {} linked to {}\n".format(
+                                    linkee_event.unique_id,
+                                    linker_event.unique_id
+                                )
+                            )
+                    except ObjectDoesNotExist:
+                        self.stdout.write(
+                            "Linkee event not found! ID {}".format(
+                                linkee_id)
+                        )
+
+    def parse_resource_csv_line(self, line) -> None:
+        if len(line[0]) > 0 and line[0].startswith("R"):
+            resource_id = line[0]
+            filename = line[1]
+            try:
+                resource = FieldTimelineResource.objects.get(
+                    identifier=resource_id
+                )
+            except ObjectDoesNotExist:
+                # New resource
+                resource = FieldTimelineResource(
+                    identifier=resource_id
+                )
+            resource.filename = filename
+            resource.title = line[3]
+            resource.date = line[4]
+
+            resource.bibliographic_citation = line[15]
+            # Lookup Dublin Core rights
+            rights = DublinCoreRights.objects.filter(
+                shorthand=line[8]
+            )
+            if rights and rights.count() > 0:
+                resource.rights = rights[0]
+            else:
+                right, created = DublinCoreRights.objects.get_or_create(
+                    shorthand=line[8],
+                    statement=''
+                )
+                resource.rights = right
+            resource.save()
+
+            # Add creator/agents
+            if line and len(line) > 5 and len(line[5]) > 0:
+                # Add creator as DC Agent
+                creator, create = DublinCoreAgent.objects.get_or_create(
+                    full_name=line[5]
+                )
+                resource.creators.add(creator)
+            # Add attached resource (currently only images)
+            try:
+                with open(
+                        settings.MEDIA_ROOT
+                        + settings.TIMELINE_IMAGE_FOLDER
+                        + '/' + filename + '.'
+                        + settings.TIMELINE_IMAGE_FORMAT,
+                        'rb'
+                ) as f:
+                    image = Image(
+                        title=line[0],
+                        file=ImageFile(File(f), name=filename)
+                    )
+                    image.save()
+                    if image and resource:
+                        ri = FieldTimelineResourceImage(
+                            resource=resource,
+                            image=image
+                        )
+                        ri.save()
+            except FileNotFoundError:
+                self.stdout.write('File {} not found!'.format(filename))
+
+    def parse_event_csv_line(self, line) -> None:
+        # check for event id
+        if len(line) >= self.min_row_length and len(line[1]) > 0:
+            event_id = line[1]
+            # Make sure this is an event id
+            if line[1].startswith("F"):
+                headline = line[6]
+                text = line[7]
+                tier = 0
+                start_year = 0
+                end_year = 0
+                who = line[0]
+                category_name = line[9]
+                resource_id = line[11]
+                category = None
+                resource = None
+                # Get/Create event category and resource
+                if len(category_name) > 0:
+                    category = create_category(category_name)
+                if len(resource_id) > 0:
+                    try:
+                        resource = FieldTimelineResource.objects.get(
+                            identifier=resource_id
+                        )
+                    except ObjectDoesNotExist:
+                        self.stdout.write(
+                            'Resource in event not found: {}'.format(
+                                resource_id)
+                        )
+                if len(line[5]) > 0:
+                    try:
+                        if '-' in line[5]:
+                            years = line[5].split('-')
+                            start_year = int(years[0])
+                            end_year = int(years[1])
+                        else:
+                            start_year = int(line[5])
+                    except ValueError:
+                        self.stdout.write('Bad year! {}'.format(line[5]))
+                if len(line[10]) > 0:
+                    try:
+                        tier = int(line[10])
+                    except ValueError:
+                        self.stdout.write('Bad tier! {}'.format(line[10]))
+                event, created = FieldTimelineEvent.objects.get_or_create(
+                    unique_id=event_id,
+                    tier=tier,
+                    headline=headline,
+                    text=text,
+                    start_date_year=start_year,
+                    end_date_year=end_year,
+                    who=who,
+                    additional_information=line[22]
+                )
+                if created:
+                    self.events_created += 1
+                else:
+                    self.events_updated += 1
+
+                # Attach category/event if present
+                if category or resource:
+                    if category:
+                        event.category = category
+                    if resource:
+                        event.resource = resource
+                if category or resource or created is False:
+                    event.save()
+
+                # If there are linked events, add them to the dict
+                event_link_string = line[24]
+                if event_link_string and len(event_link_string) > 0:
+                    events_links = event_link_string.split(';')
+                    self.event_links[event_id] = events_links
+
+                # If the line has oral history information
+                # create object and link it to event
+                if len(line[16]) > 0:
+                    oral_history, created = \
+                        FieldOralHistory.objects.get_or_create(
+                            timeline_event=event,
+                            speaker=line[16],
+                            place=line[17],
+                            era=line[18],
+                            text=line[19]
+                        )
+                    # Add rights if the exist
+                    if oral_history and len(line[19]) > 0:
+                        shorthand = line[19]
+                        if 'Rights:' in shorthand:
+                            shorthand = shorthand.replace('Rights:')
+                        right, created = \
+                            DublinCoreRights.objects.get_or_create(
+                                shorthand=shorthand.lstrip()
+                            )
+                        oral_history.rights = right
+                        oral_history.save()
+
+    def add_arguments(self, parser) -> None:
+        # csv file to parse
+        parser.add_argument('csvfile', type=argparse.FileType('r'))
+        parser.add_argument('resourcecsvfile', type=argparse.FileType('r'))
+        parser.add_argument('rightsfile', type=argparse.FileType('r'))
+
+    def handle(self, *args, **options):
+        # import rights tab
+        self.import_rights(options['rightsfile'])
+        resourcecsvfile = csv.reader(
+            options['resourcecsvfile'], delimiter=',')
+        csvfile = csv.reader(
+            options['csvfile'], delimiter=',')
+
+        # import DV resources from tab
+        for resource_csv_line in resourcecsvfile:
+            self.parse_resource_csv_line(resource_csv_line)
+
+        # Import events
+        x = 0
+        for csv_line in csvfile:
+            self.parse_event_csv_line(csv_line)
+            x += 1
+            print("{}\n".format(x))
+
+        # Add event links
+        if self.event_links and len(self.event_links) > 0:
+            self.link_events()
+        self.stdout.write("{} Events created, {} updated".format(
+            self.events_created,
+            self.events_updated
+        ))
--- field_wagtail/models.py
+++ field_wagtail/models.py
@@ -1,5 +1,10 @@
-# from django.db import models
+import json
 import wagtail
+from django_kdl_timeline.models import (
+    AbstractTimelineEventSnippet,
+    AbstractDublinCoreWagtailMediaResource
+)
+from wagtail.images.models import (Image)
 from django import forms
 from django.conf import settings
 from django.db import models
@@ -12,22 +17,11 @@ from wagtail.admin.edit_handlers import (FieldPanel)
 from wagtail.contrib.routable_page.models import route, RoutablePageMixin
 from wagtail.core.models import (Page)
 # from wagtail.images.edit_handlers import ImageChooserPanel
-from wagtail.search import index
 from wagtail.snippets.models import register_snippet
-from field_timeline.models import (FieldTimelineEvent)
+
 register_snippet(DublinCoreAgent)
 register_snippet(DublinCoreRights)
-
-
 # register_snippet(FieldTimelineEvent)
-
-
-@register_snippet
-class FieldTimelineEventSnippet(index.Indexed, FieldTimelineEvent):
-    search_fields = [
-        index.SearchField('headline', partial_match=True),
-        index.SearchField('text', partial_match=True),
-    ]
 
 
 class HomePage(BaseRichTextPage):
@@ -139,9 +133,156 @@ class MailingListFormPage(RoutablePageMixin, BasePage):
         )
 
 
-class AbstractDublinCoreResourcePage(Page):
-    """ Abstract Page to present a resource as a stand alone detail page
-        Also useful to allow a friendlier way of editing the resource"""
+class FieldTimelineResource(AbstractDublinCoreWagtailMediaResource):
+    # resource_id = models.CharField(max_length=256, blank=False, null=False)
+    filename = models.CharField(max_length=256, blank=False, null=False,
+                                default='')
+    image_ref = models.CharField(max_length=256, blank=True)
+    # rights = models.CharField(max_length=64, blank=False, null=True)
+    # date = models.CharField(max_length=64, blank=False, null=True)
+    caption = models.TextField(blank=True, default='')
+    # photographer = models.CharField(max_length=256, blank=True)
+    credit = models.CharField(max_length=256, blank=True)
+    link_url = models.CharField(max_length=512, null=True)
+    resource_type = models.CharField(
+        max_length=64, choices=[
+            ('I', 'IMAGE'),
+            ('D', 'DOCUMENT'),
+            ('A', 'AUDIO'),
+            ('V', 'VIDEO')],
+        default='I'
+    )
 
-    class Meta:
-        abstract = True
+    def to_timeline_media(self):
+        # todo REFACTOR to use attached image, DC fields
+        """Transform to a timelineJS media object"""
+        url = self.url
+        # if len(url) == 0:
+        #     # Add default resource name
+        #     url = '{}.{}'.format(
+        #         TIMELINE_IMAGE_FOLDER + '/' + self.filename,
+        #         TIMELINE_IMAGE_FORMAT)
+        # newimage = myimage.get_rendition('fill-300x150|jpegquality-60')
+        media_data = {'url': url, 'thumbnail': url, }
+        # if len(self.caption) > 0:
+        #     media_data['caption'] = self.caption
+        # if len(self.photographer) > 0 and len(self.credit) > 0:
+        #     media_data['credit'] = "{} / {}".format(
+        #         self.photographer,
+        #         self.credit
+        #     )
+        # elif len(self.photographer) > 0:
+        #     media_data['credit'] = "{}".format(
+        #         self.photographer,
+        #     )
+        # elif len(self.credit) > 0:
+        #     media_data['credit'] = self.credit
+        # if self.link_url and len(self.link_url) > 0:
+        #     media_data['link'] = self.link_url
+        return media_data
+
+    def __str__(self):
+        return "{}:{}, {}".format(
+            self.resource_id,
+            self.caption,
+            self.link_url
+        )
+
+
+class FieldTimelineCategory(models.Model):
+    category_name = models.CharField(max_length=256, blank=True)
+
+    def __str__(self):
+        return self.category_name
+
+
+class FieldOralHistory(models.Model):
+    timeline_event = models.ForeignKey(
+        'FieldTimelineEvent',
+        on_delete=models.PROTECT, null=True
+    )
+    speaker = models.CharField(
+        max_length=256, blank=False, null=False, default=''
+    )
+    place = models.CharField(
+        max_length=256, blank=False, null=False, default=''
+    )
+    era = models.CharField(
+        max_length=128, blank=False, null=False, default=''
+    )
+    text = models.TextField(blank=True, default='')
+    rights = models.ForeignKey(
+        DublinCoreRights,
+        null=True, blank=True,
+        on_delete=models.SET_NULL
+    )
+
+
+"""An event in the FIELD timeline, inherited from slide"""
+
+
+class FieldTimelineEvent(AbstractTimelineEventSnippet):
+    category = models.ForeignKey('FieldTimelineCategory',
+                                 on_delete=models.PROTECT, null=True)
+    tier = models.IntegerField(null=False, blank=False, default=0)
+    resource = models.ForeignKey('FieldTimelineResource',
+                                 on_delete=models.CASCADE, null=True)
+    linked_events = models.ManyToManyField('self')
+    who = models.CharField(max_length=256, blank=False, null=False, default='')
+    additional_information = models.TextField(blank=True, default='')
+    # css class for in-timeline links
+    ev_target_class = "timeline_link"
+
+    ordering = ['-start_date_year']
+
+    def get_timeline_data(self):
+        data = super().get_timeline_data()
+        # Add extra data from fieldtimeline
+        if self.category:
+            data['group'] = self.category.category_name
+        if self.resource:
+            data['media'] = self.resource.to_timeline_media()
+        # If there's a resource, add it here
+        if self.linked_events and self.linked_events.count() > 0:
+            text = data['text']['text']
+            text += "<br/>Related Events: "
+            x = 0
+            for linked_event in self.linked_events.all():
+                if (x > 0):
+                    text += ", "
+                    text += ("<a href=\"#\" class=\"{}\" "
+                             "data-unique-id=\"{}\">{}</a>").format(
+                        self.ev_target_class,
+                        linked_event.unique_id,
+                        linked_event.headline
+                    )
+                x += 1
+            data['text']['text'] = text
+        return data
+
+    def to_timeline_json(self):
+        data = self.get_timeline_data()
+        if self.resource:
+            data['media'] = self.resource.to_timeline_media()
+        return json.dumps(data)
+
+    def __str__(self):
+        return "{}:{}, {}".format(
+            self.unique_id,
+            self.start_date_year,
+            self.headline
+        )
+
+
+class FieldTimelineResourceImage(models.Model):
+    resource = models.ForeignKey(
+        'FieldTimelineResource',
+        on_delete=models.CASCADE,
+        null=True,
+    )
+
+    image = models.ForeignKey(
+        Image,
+        on_delete=models.CASCADE,
+        null=True
+    )
