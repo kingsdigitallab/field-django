4b3a05dc5788ace05578722add351f2d46b4daad
elliotthall <elliott.hall@kcl.ac.uk>
2020-07-22 09:55:51 +0000

    hollow out field_timeline

--- field_timeline/urls.py
+++ field_timeline/urls.py
@@ -1,9 +1,7 @@
-from django.urls import path, re_path
-
-from field_timeline import views
+# from field_timeline import views
 
 urlpatterns = [
-    path('json/', views.TimelineJSONView.as_view(),
-         name='timeline_json'),
-    re_path(r'^$', views.TimelineTemplateView.as_view(), name='timeline')
+    # path('json/', views.TimelineJSONView.as_view(),
+    #      name='timeline_json'),
+    # re_path(r'^$', views.TimelineTemplateView.as_view(), name='timeline')
 ]
