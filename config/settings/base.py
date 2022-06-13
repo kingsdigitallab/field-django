2fdf6117dc544cfcb8abf520d0dcb3f75f6c70fe
Geoffroy Noël <geoffroy-noel-ddh@users.noreply.github.com>
2022-06-13 22:07:41 +0100

    Disabled broken link middleware to reduce daily spam

--- config/settings/base.py
+++ config/settings/base.py
@@ -175,7 +175,7 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.common.BrokenLinkEmailsMiddleware",
+    # "django.middleware.common.BrokenLinkEmailsMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "wagtail.contrib.redirects.middleware.RedirectMiddleware",
 ]
