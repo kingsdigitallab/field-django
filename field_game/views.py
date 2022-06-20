6517bcbd9b54a934e6f8f515389f12f8ed3d661f
Elliott Hall <elliott.hall@kcl.ac.uk>
2022-06-20 18:47:58 +0100

    API bug fix

--- field_game/views.py
+++ field_game/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from field_game.models import FieldGame, GameEvent, Farmer
 from field_game.serializers import (
     FieldGameSerializer,
@@ -7,26 +8,29 @@ from field_game.serializers import (
 from rest_framework import permissions
 from rest_framework import viewsets
 
+# to toggle between easy for debug and production
+if settings.DEBUG:
+    API_PERMISSIONS = [permissions.AllowAny]
+else:
+    API_PERMISSIONS = [permissions.IsAuthenticatedOrReadOnly]
 
-class FieldGameViewSet(viewsets.ViewSet):
+
+class FieldGameViewSet(viewsets.ModelViewSet):
     """ Serve a new instance of the Field boardgame with relevant settings"""
-
     queryset = FieldGame.objects.all()
     serializer_class = FieldGameSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = API_PERMISSIONS
 
 
-class FarmerViewSet(viewsets.ViewSet):
+class FarmerViewSet(viewsets.ReadOnlyModelViewSet):
     """ Serve a new instance of the Field boardgame with relevant settings"""
-
     queryset = Farmer.objects.all()
     serializer_class = FarmerSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = API_PERMISSIONS
 
 
-class GameEventViewSet(viewsets.ViewSet):
+class GameEventViewSet(viewsets.ModelViewSet):
     """ Serve a new instance of the Field boardgame with relevant settings"""
-
     queryset = GameEvent.objects.all()
     serializer_class = GameEventSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = API_PERMISSIONS
