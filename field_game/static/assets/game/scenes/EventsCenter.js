6ec14508891bc050a4fde4956d6468a810062a7d
Elliott Hall <elliott.hall@kcl.ac.uk>
2023-11-30 10:45:53 +0000

    reorganize apps into volumes

--- field_game/static/assets/game/scenes/EventsCenter.js
+++
@@ -1,8 +0,0 @@
-/*jshint esversion: 8 */
-/**
- * Central event system for the game
-inspired by:
- https://blog.ourcade.co/posts/2020/phaser3-how-to-communicate-between-scenes/
- */
-const eventsCenter = new Phaser.Events.EventEmitter();
-export default eventsCenter;
--- field_wagtail/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from field_wagtail.models import FieldImage, FieldImageRendition
-# Register your models here.
-admin.register(FieldImage)
-admin.register(FieldImageRendition)
